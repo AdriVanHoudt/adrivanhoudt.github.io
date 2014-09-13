41a82aa033e38463d49b7136cc857211757bb192
Adri Van Houdt <adri.van.houdt@gmail.com>
2014-09-14 00:08:08 +0200

    adde basic grunt closes #4

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,49 +2,104 @@ module.exports = function(grunt) {
     
     grunt.initConfig({
         clean:  {
-            build:  ['./build']
+            build:  './build',
+            dev:    ['./build/css', './build/scrips']
+        },
+        copy:   {
+            analytics:   {
+                expand: true,
+                cwd:    './src/scripts/',
+                src:    'analytics.js',
+                dest:   './build/scripts/'    
+            }
         },
         imagemin:   {
             png:    {
                 options:    {
                     optimizationLevel:  7
                 },
-                files:  [{
-                    expand: true,
-                    cwd:    './img/',
-                    src:    ['**/*.png'],
-                    dest:   './build/img/',
-                    ext:    '.png'
-                }]
+                expand: true,
+                cwd:    './src/img/',
+                src:    '**/*.png',
+                dest:   './build/img/',
+                ext:    '.png'
             },
              jpg:    {
                  options:   {
                      progressive:   true
                 },
-                files:  [{
-                    expand: true,
-                    cwd:    './img/',
-                    src:    ['**/*.jpg'],
-                    dest:   './build/img/',
-                    ext:    '.jpg'
-                }]
+                expand: true,
+                cwd:    './src/img/',
+                src:    '**/*.jpg',
+                dest:   './build/img/',
+                ext:    '.jpg'
             }
         },
         autoprefixer:   {
-            build: {
-                files:    [{
-                    expand: 'true',
-                    cwd:    './css/',
-                    src:    ['**/*.css'],
-                    dest:   './build/css/',
-                }]
+            main: {
+                expand: 'true',
+                cwd:    './src/css/',
+                src:    '**/*.css',
+                dest:   './build/css/'
+            }
+        },
+        cssmin: {
+            build:  {
+                options:    {
+                    banner: "/* Minified by grunt */"
+                },
+                expand: true,
+                cwd:    './build/css/',
+                src:    ['*.css', '!*.min.css'],
+                dest:   './build/css/'  
+            }
+        },
+        uncss:  {
+            main: {
+                src:    'index.html',
+                dest:   './build/css/main.css'
+          }
+        },
+        htmlmin:    {
+            index:  {
+                options:    {
+                    removeComments:             true,
+                    collapseWhitespace:         true,
+                    removeRedundantAttributes:  true,
+                    useShortDoctype:            true,
+                    removeAttributeQuotes:      true
+                },
+                files:  {
+                    'index.html':   'src/html/index.html'
+                }
             }
         }
     })
     
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-uncss');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
     
-    grunt.registerTask('default', ['clean:build','imagemin', 'autoprefixer:build']);
+    grunt.registerTask('dev', [
+        'clean:dev',
+        'copy:analytics',
+        'autoprefixer',
+        'uncss',
+        'cssmin:build',
+        'htmlmin:index'
+    ])
+    
+    grunt.registerTask('default', [
+        'clean:build',
+        'copy:analytics',
+        'imagemin',
+        'autoprefixer',
+        'uncss',
+        'cssmin:build',
+        'htmlmin:index'
+    ]);
 };
