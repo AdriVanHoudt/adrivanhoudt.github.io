695d540a6647128ef90bd25030ca736783cec4d9
Adri Van Houdt <adri.van.houdt@gmail.com>
2014-09-16 13:22:48 +0200

    fix dev grunt task // added lang to html tag

--- Gruntfile.js
+++ Gruntfile.js
@@ -95,7 +95,7 @@ module.exports = function(grunt) {
     ])
     
     grunt.registerTask('default', [
-        'clean:build',
+        'clean:dev',
         'copy:analytics',
         'autoprefixer',
         'uncss',
